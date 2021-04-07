a85782830db096efd31796c665e2cbaa58d91eb2
Niharika Pandit <niharikap219@gmail.com>
2021-04-07 11:27:41 +0530

    form validations

--- crypto/src/app/login/login.component.ts
+++ crypto/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+  loginForm:any;
+  email:any;
+  password:any;
 
   ngOnInit(): void {
+    this.loginForm= this.fb.group({
+      email:['',[Validators.required, this.validateEmail]],
+      password:['',[Validators.required,this.validatePwd]]
+    })
   }
   hide = true;
 
+  validateEmail(c:FormControl){
+    let re=/^\S+@\S+$/;
+    return re.test(c.value)?null:{
+      emailError:"Email is invalid"
+    }
+  }
+
+  validatePwd(c:FormControl){
+    let re=/[a-zA-Z]*[0-9]{1}/;
+    return re.test(c.value)?null:{
+      pwdError:"Password should have atleast 1 number"
+    }
+  }
 }
--- crypto/src/app/register/register.component.ts
+++ crypto/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
+  registerForm:any;
   ngOnInit(): void {
+    this.registerForm= this.fb.group({
+      name:['',[Validators.required,Validators.minLength(4)]],
+      email:['',[Validators.required, this.validateEmail]],
+      password:['',[Validators.required,this.validatePwd]],
+      dob:['',[Validators.required,this.validateDOB]]
+    })
   }
   hide:boolean=true;
+
+  validateEmail(c:FormControl){
+    let re=/^\S+@\S+$/;
+    return re.test(c.value)?null:{
+      emailError:"Email is invalid"
+    }
+  }
+
+  validatePwd(c:FormControl){
+    let re=/[a-zA-Z]*[0-9]{1}/;
+    return re.test(c.value)?null:{
+      pwdError:"Password should have atleast 1 number"
+    }
+  }
+
+  validateDOB(c:FormControl){
+    let today= new Date();
+    let date= new Date(c.value);
+    console.log(today,date)
+    if(date>=today) return {dobError:"DOB should be less than today's date."}
+    else return null
+  }
 }
